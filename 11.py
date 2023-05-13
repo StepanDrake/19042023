bf28f39e33dfe5ae88e97034e6787ade5230aa79
Your Name <you@example.com>
2023-05-13 17:43:51 +0300

    w10

--- 11.py
+++
@@ -1,8 +0,0 @@
-#task11
-text = input('Введіть будь-який текст >>>')
-text_result = []
-for element in text:
-    text_result.append(element)
-    if element.isupper():
-        text_result == element
-print(text_result)
