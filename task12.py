4fb9578f0d0e6203148592edb5b8851eec4682c5
Your Name <you@example.com>
2023-05-13 17:48:55 +0300

    clean master

--- task12.py
+++
@@ -1,4 +0,0 @@
-#Task 12 fahrenheit to celsius tempf=(9/5)*tempc-32
-temperature_celsius = float(input('Введіть температуру Цельсія >>>'))
-temperature_fahrenheit = round((9/5)*temperature_celsius+32, 1)
-print('Перевести по целсьію у фарангейт>>>', temperature_fahrenheit)
