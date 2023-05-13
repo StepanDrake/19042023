fc514a01a01d0eb882416188e9ec3d18ccab6581
Your Name <you@example.com>
2023-05-13 17:56:13 +0300

    i did hW-10

--- t10_1.py
+++ t10_1.py
@@ -1,1 +1,7 @@
-print('ij;jk')
+cities_last_ten_years = set(input('Введіть міста в яких ви побували за минулі 10 років через пробіл>>>').title().split())
+сities_you_wish_visit = set(input('Введіть міста в яких ви хочете побувати за наступні 10 років >>>').title().split())
+like_cities = cities_last_ten_years.intersection(сities_you_wish_visit)
+if like_cities:
+    print('мабуть, вам дуже сподобалося в містах, які ви повторили>>>', like_cities)
+else:
+    print('Співпадіння відсутні')
